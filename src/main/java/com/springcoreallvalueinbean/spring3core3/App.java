ff89f1da16753e848ede2ccb1e4946ea479e331e
Abhishek Aryan <bittoogoswami@gmail.com>
2024-05-28 23:47:07 +0530

    Update App.java

--- src/main/java/com/springcoreallvalueinbean/spring3core3/App.java
+++ src/main/java/com/springcoreallvalueinbean/spring3core3/App.java
@@ -12,6 +12,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+	    
       ApplicationContext  context = new ClassPathXmlApplicationContext("config.xml");
       Employee employee1 = (Employee) context.getBean("employee1");
       System.out.println(employee1);
